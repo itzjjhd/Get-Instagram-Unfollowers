b66fc30190b0a9a35903484b1db45fb53b6dbb76
JotJot <33303126+itzjjhd@users.noreply.github.com>
2019-04-23 17:30:23 +0200

    Update Get_instagram_followers.py

--- Get_instagram_followers.py
+++ Get_instagram_followers.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# Use text editor to edit the script and type in valid Instagram username/password
 from InstagramAPI import InstagramAPI
 import linecache
 
